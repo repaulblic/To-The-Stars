d48ece2440a9fbb6739d4994595950a5e6bf9801
Paul James <pespitia@uci.edu>
2015-11-11 10:42:57 -0800

    Added main menu!

---
+++ Assets/Scripts/EarthMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthMove : MonoBehaviour {
+
+	public float speed;
+	Vector3 movement;
+	Vector3 movement2;
+	Vector3 d;
+	
+	
+	private Rigidbody rb;
+	
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		transform.Rotate(Vector3.down * Time.deltaTime * speed);
+		
+	}
+
+}
---
+++ Assets/Scripts/Moveonclick.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveonclick : MonoBehaviour {
+
+
+
+	bool run = false;
+	bool loaded = false;
+	//bool finished = false;
+	bool started;
+	Animation ani;
+
+
+	void Start(){
+		ani = GetComponent<Animation> ();
+	}
+
+
+	 void Update()
+	{
+		if (Input.anyKeyDown & !run) {
+			started = true;
+			StartCoroutine("animat");
+			run = true;
+		}
+
+		if (!loaded & !ani.isPlaying & started) {
+			Application.LoadLevelAdditive (1);
+			loaded = true;
+		}
+
+
+	}
+
+	IEnumerator animat() {
+		ani.Play (ani.clip.name);
+		DestroyObject(GameObject.Find("AnyButton"));
+		yield return null;
+	}
+
+}
