2b110ffe973ed0e3a806e945d1168d20de97d958
yaox8 <xuyao9188@gmail.com>
2015-11-17 19:38:49 -0800

    ddd

---
+++ Assets/Planet/OctahedronSphere.cs
@@ -0,0 +1,13 @@
+﻿	using UnityEngine;
+
+	[RequireComponent(typeof(MeshFilter))]
+	public class OctahedronSphere : MonoBehaviour {
+
+		public int subdivisions = 6;
+		
+		public float radius = 1f;
+		
+		private void Awake () {
+			GetComponent<MeshFilter>().mesh = OctahedronSphereCreator.Create(subdivisions, radius);
+		}
+	}
