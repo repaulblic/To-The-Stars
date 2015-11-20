da5ff911b0a5c78ef0555f9dc3ebfb58bddd6e9b
Paul James <pespitia@uci.edu>
2015-11-20 02:19:42 -0800

    Added the preliminary planet selection screen. Currently only the right arrow works, the left arrow is buggy.

---
+++ Assets/Scripts/PlanetPan.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetPan : MonoBehaviour {
+
+
+	public float speed = 2F;
+	int rotAmount = 45;
+	Vector3 destEuler = Vector3.zero;
+	Vector3 currEuler;
+
+
+	void Start () {
+
+		currEuler = destEuler;
+		transform.eulerAngles = destEuler;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+
+			destEuler.y += rotAmount;
+
+		}
+		if (Input.GetAxis("Horizontal") < 0) {
+			destEuler.y -= rotAmount;
+		}
+
+		currEuler = Vector3.Slerp(currEuler, destEuler, Time.deltaTime * speed);
+		transform.eulerAngles = currEuler;
+	}
+}
