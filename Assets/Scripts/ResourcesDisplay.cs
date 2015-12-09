b31c43d42d1df95109e7e9c926991aec3211429e
xueguangl23 <xueguanl@uci.edu>
2015-12-08 17:40:01 -0800

    add a scene to display resources, haven't linked together yet. Also, randomly gain resources from the wood boxes.

---
+++ Assets/Scripts/ResourcesDisplay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ResourcesDisplay : MonoBehaviour {
+	public Text experienceText;
+	public Text metalText;
+	public Text waterText;
+	public Text iceText;
+	public Text fireText;
+	public Text woodText;
+	// Use this for initialization
+	void Start () {
+		GameObject theGame = GameObject.Find("GameManager");
+		GameControl game = theGame.GetComponent<GameControl>();
+		experienceText.text = "Experience: "+game.experience.ToString();
+		metalText.text = "Metal Resources: " + game.metal.ToString ();
+		waterText.text = "Water Resources: " + game.water.ToString ();
+		iceText.text = "Ice Resources: " + game.ice.ToString ();
+		fireText.text = "Fuel Resources: " + game.fire.ToString ();
+		woodText.text = "Wood Resources: " + game.wood.ToString ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
