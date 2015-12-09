a5be4dae14746e64515fde9ce435d2a10788e34f
Yuan Xie <yxie4@uci.edu>
2015-12-09 07:34:33 -0800

    Enemy now can chase player

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,15 +5,20 @@ public class EnemyMovement : MonoBehaviour {
 
 	Transform player;
 	NavMeshAgent nav;
+	Transform enemy;
+	int moveSpeed = 3;
+	int rotationSpeed = 3;
 
 	void Awake () {
+		enemy = GetComponent<Transform>();
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
 		nav = GetComponent<NavMeshAgent> ();
 	}
-
+		
 	//Navigate toward player
 	void Update () {
 		nav.SetDestination (player.position);
-
+		enemy.rotation = Quaternion.Slerp (enemy.rotation, Quaternion.LookRotation (player.position - enemy.position), rotationSpeed* Time.deltaTime);
+		enemy.position += enemy.forward * moveSpeed * Time.deltaTime;
 	}
 }
